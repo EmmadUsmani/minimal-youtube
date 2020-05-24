ddfa2e825df662b4116a848ad10da501a7422645
emmadusmani <emmadausmani@gmail.com>
2020-05-24 08:22:03 -0700

    added theme and primary color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 import "./index.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#5e35b1",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
