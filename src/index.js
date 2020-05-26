3861f6b1d4ded974708e45abde5713960c52c43e
emmadusmani <emmadausmani@gmail.com>
2020-05-26 10:11:52 -0700

    uninstalled gh-pages, removing deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
