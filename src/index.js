837276519936934a853fce42fd4c00f1fd77ebd8
emmadusmani <emmadausmani@gmail.com>
2020-05-26 09:40:44 -0700

    deploying to github pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
